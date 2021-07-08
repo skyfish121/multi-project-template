08237dc489428d65ada01966e699d8476e4962fc
Sugawara Masahiko <sugawaramasahiko@sandon.jp>
2021-07-08 09:37:10 +0900

    起動部分作成

---
+++ src/test/java/com/sandon/multi/api/MultiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandon.multi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
